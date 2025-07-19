8124c91f66ebda028bd7853ca65fbd7ed93a1750
SASIDHARUN M <sasidharun0710@gmail.com>
2025-07-19 13:37:11 +0530

    Update remove_duplacted_elements_inarray.java

--- remove_duplacted_elements_inarray.java
+++ remove_duplacted_elements_inarray.java
@@ -54,3 +54,30 @@ public class MyJava {
     }
 }
 */
+package KANNAN;
+
+public class remove_dup {
+
+	public static void main(String[] args) {
+
+		int arr[] = {1,2,2,33,44,44,33,45,756,33};
+		int visit = -1;
+		for(int i = 0;i<arr.length;i++)
+		{
+			if(arr[i] != -1)
+			{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if(arr[i] == arr[j])
+				{
+					arr[j] = visit;
+				}
+			}
+			System.out.print(arr[i]+" ");
+}
+
+		}
+	}
+
+}
+
